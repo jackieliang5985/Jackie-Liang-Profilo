3dfc3e414f7fd784bddd64cb1224bb08433798ca
jackieliang5985 <dinowoolly79@gmail.com>
2025-11-18 05:40:21 -0500

    holy pages not working

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const isStaticExport = process.env.NEXT_EXPORT === 'true'
 
+const repoName = 'Profilo'
+
 const nextConfig = {
   reactStrictMode: true,
   output: isStaticExport ? 'export' : 'standalone', // Standalone for server deploys, export for GitHub Pages
+  basePath: isStaticExport ? `/${repoName}` : undefined,
+  assetPrefix: isStaticExport ? `/${repoName}/` : undefined,
   images: {
     domains: ['avatars.githubusercontent.com', 'github.com'],
     unoptimized: isStaticExport,
